56d5cea7671efa986eec3eb40694aecf1a6d9b38
PragyaJ25 <134546767+PragyaJ25@users.noreply.github.com>
2024-01-28 22:25:23 +0530

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,21 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   grabCursor: true,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
